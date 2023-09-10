d505ae7b9ad7d9e2a7f26cfe4800fbf1cd84401a
NPotapov <nikita.potapov@develonica.ru>
2023-09-10 20:44:02 +0300

    Fixed code smells

--- src/main/java/ru/develonica/load/testing/web/security/entity/Role.java
+++ src/main/java/ru/develonica/load/testing/web/security/entity/Role.java
@@ -1,6 +1,4 @@
 package ru.develonica.load.testing.web.security.entity;
 
-import org.springframework.security.core.GrantedAuthority;
-
 public class Role {
 }
--- src/main/java/ru/develonica/load/testing/web/test/service/TestService.java
+++ src/main/java/ru/develonica/load/testing/web/test/service/TestService.java
@@ -3,6 +3,7 @@ package ru.develonica.load.testing.web.test.service;
 import org.springframework.stereotype.Service;
 import ru.develonica.load.testing.web.test.model.exception.TestCreateException;
 import ru.develonica.load.testing.web.test.model.request.TestCreateRequest;
+import ru.develonica.load.testing.web.test.model.request.validator.TestRequestValidator;
 
 import java.util.UUID;
 
@@ -10,6 +11,11 @@ import java.util.UUID;
 public class TestService {
 
     public UUID createTestTask(TestCreateRequest testCreateRequest) throws TestCreateException {
-        return UUID.randomUUID();
+        if (TestRequestValidator.validate(testCreateRequest)) {
+            return UUID.randomUUID();
+        } else {
+            throw new TestCreateException(new TestCreateException("URL is malformed"));
+        }
+
     }
 }
